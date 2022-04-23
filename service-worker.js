18e9112a9bdee8611407cdacd709ddbb2aa672e7
GamerNoTitle <GamerNoTitle@outlook.com>
2022-04-23 09:21:23 +0800

    Upgrade

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e498f880d9949ac19f2793493054d05b.js"
+  "precache-manifest.9afa2167110700e32224e67f17027e58.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "unlock-music"});
